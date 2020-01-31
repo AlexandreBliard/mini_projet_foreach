0e5d3c5e10d94b708523c79f408ffc2aa299f596
AlexandreBliard <alexandre.bliard@le-campus-numerique.fr>
2020-01-31 15:13:12 +0100

    troisième partie

--- function.php
+++ function.php
@@ -1,20 +1,23 @@
 <?php
 // UTILISER LA VARIABLE $song pour afficher les données
 
-foreach ($song as $refrain){
-    if (is_array($refrain)){
-        ?> <br> <?php
-        foreach ($refrain as $refrains => $test){
-            echo $test ."<br>";
-    }
-        ?> <br> <?php
-    }else
-    {
-        echo $refrain .'<br>';
-    }
-
-    /*
-
-        var_dump($test);
-    }*/
+// fonction pour afficher les tableaux
+function afficheCouplet($tab, $precis){
+    foreach ($tab[$precis] as $goon){
+        echo $goon ."<br>";
+    }echo "<br>";
 }
+function afficheLigne($tab, $cle){
+    echo $tab [$cle] ;
+    
+}
+
+/*===============code======================*/
+afficheLigne($song, 'title');
+echo "<br>";
+echo "<br>";
+afficheCouplet($song,'chorus');
+afficheCouplet($song,'first_verse');
+afficheCouplet($song,'chorus');
+afficheCouplet($song,'second_verse');
+afficheLigne($song, 'author');
