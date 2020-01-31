de4393bc5777587a7254257c045c279407a99cd0
AlexandreBliard <alexandre.bliard@le-campus-numerique.fr>
2020-01-31 13:44:41 +0100

    deuxième partie

--- function.php
+++ function.php
@@ -1,5 +1,20 @@
 <?php
 // UTILISER LA VARIABLE $song pour afficher les données
+
 foreach ($song as $refrain){
-    echo $refrain .'<br>';
+    if (is_array($refrain)){
+        ?> <br> <?php
+        foreach ($refrain as $refrains => $test){
+            echo $test ."<br>";
+    }
+        ?> <br> <?php
+    }else
+    {
+        echo $refrain .'<br>';
+    }
+
+    /*
+
+        var_dump($test);
+    }*/
 }
