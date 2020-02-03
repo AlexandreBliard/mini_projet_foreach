bd479afc5c88f293f376b13662e05e67d577fea4
AlexandreBliard <alexandre.bliard@le-campus-numerique.fr>
2020-02-03 13:26:30 +0100

    etape 8 check

--- function.php
+++ function.php
@@ -9,6 +9,12 @@ function afficheLigne($tab, $cle) {//affichage de ligne
 
 
 function afficheRefrain($tab){//affichage du tableau
+    foreach ($tab as $ligne){//parcours le tableau
+        echo $ligne . '<br>';// affichage résultat et retour ligne
+        }echo '<br>'; //retour ligne de fin de paragraphe
+    }
+
+function afficheCouplet($tab){//affichage du tableau
     foreach ($tab as $key => $ligne){// création de la variable
         if ($key%2 ){//modulo pour faire comparaison pair ou impair
             echo $ligne . '<br>';// affichage résultat et retour ligne
@@ -18,14 +24,13 @@ function afficheRefrain($tab){//affichage du tableau
 
 
 
-
 /*========================== CODE =============================*/
 afficheLigne($song, 'title');
 echo "<br>";
 echo "<br>";
 afficheRefrain($song['data']['chorus_1']);//recherche du bon tableau
-afficheRefrain($song['data']['first_verse']);
+afficheCouplet($song['data']['first_verse']);
 afficheRefrain($song['data']['chorus_1']);
-afficheRefrain($song['data']['second_verse']);
+afficheCouplet($song['data']['second_verse']);
 afficheLigne($song, 'author');
 
